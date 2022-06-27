b4c79a60493f063e2fe8b60bf57829d79d788ed2
sangho527 <null>
2022-06-27 05:02:11 +0000

    입력 , 출력

---
+++ DAY1-2/DATA_INPUT2.py
@@ -0,0 +1,23 @@
+ # DAY1
+print(" print("
+출력
+입력해주세요 : ", end='') 정수를 입력해주세요 : ", end='')
+두 한번에 출력
+두     순서 교체 후 한번에 출력
+a = input()
+ 
+b = input()
+ print(a, b) # print(b, a) #
+-
+첫번째
+두번째
+print("     2번 입력받습니다 : ", end='')
+문자 2개 입력, 공백 기준 분리, 정수형 map(int, input.split()), 실수형 map(float, input.split())
+두       한번에 출력
+한번에
+입력과
+c, d = input().split() #
+정수를
+변수를 변수를
+문자를
+ 
--- DAY1-2/HELLOWORLD.py
+++ DAY1-2/HELLOWORLD.py
@@ -1,1 +1,12 @@
 print("Hello world")
+print("\'Hello world\'")
+print('\"Hello world\"')
+print("Hello \n world")
+print("Hello    world")
+
+# 한줄 주석
+"""아
+아아
+아아
+아아아
+"""
