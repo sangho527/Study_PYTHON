5f06a0eb93dde1c271b9cd4bbd343094610290ef
sangho527 <null>
2022-06-27 06:25:23 +0000

    값/ 변환 연산자

---
+++ DAY1-2/DATA_CONVERT2.py
@@ -0,0 +1,8 @@
+#Day1 - 변환과 연산자
+
+#a,b = input('단어 여러번 출력: ').split()
+#print(a*int(b)) # 단어 반복
+
+s = int(input('문자 반복 횟수 입력 : '))
+s2 = input('문자열 직접 입력 : ')
+print(s*s2) # 문자 반복
---
+++ DAY1-2/DATA_CONVERT3.py
@@ -0,0 +1,6 @@
+#Day1 - 변환과 연산자
+f = float(input('소수점 5자리 이하 입력: '))
+print(round(f,2)) # 소수점 2자리 출력, round는 반올림 함수
+
+#a=input('소수를 5자리 이하 입력: ')
+#a=float(a) # 입력값 소수로 캐스팅
