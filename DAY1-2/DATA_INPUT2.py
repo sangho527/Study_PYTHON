34c9f2c893675159410535a3537c1c659969db5c
sangho527 <null>
2022-06-27 05:05:47 +0000

    입출력

--- DAY1-2/DATA_INPUT2.py
+++ DAY1-2/DATA_INPUT2.py
@@ -1,23 +1,10 @@
- # DAY1
-print(" print("
-출력
-입력해주세요 : ", end='') 정수를 입력해주세요 : ", end='')
-두 한번에 출력
-두     순서 교체 후 한번에 출력
+print("첫번째 정수를 입력해주세요 : ", end="")
 a = input()
- 
-b = input()
- print(a, b) # print(b, a) #
--
-첫번째
-두번째
-print("     2번 입력받습니다 : ", end='')
-문자 2개 입력, 공백 기준 분리, 정수형 map(int, input.split()), 실수형 map(float, input.split())
-두       한번에 출력
-한번에
-입력과
-c, d = input().split() #
-정수를
-변수를 변수를
-문자를
- 
+print("첫번째 정수를 입력해주세요 : ", end="")
+a = input()
+print(a,b) # 두 변수를 한번에 출력
+print(b,a) # 두 변수를 순서 교체 후 한번에 출력
+
+print("한번에 문자를 2번 입력받습니다 : ", end="")
+c,d = input().split() # 문자 2개 입력, 공백 기준 분리, 정수형 Map(int, input().split), 실수형 map(float, input().split())
+print(c,d) # 두 변수를 한번에 출력
