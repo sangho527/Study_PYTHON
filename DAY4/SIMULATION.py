917beb33fd87e39e7976c3e279613fd964277aa2
sangho527 <null>
2022-07-08 14:23:52 +0000

    구현 시뮬레이션

---
+++ DAY4/SIMULATION.py
@@ -0,0 +1,20 @@
+n = int(input('NxN 2차원 행렬을 생성(N 입력): '))
+plans = input('L, R, U, D 4가지 방향 이동 계획 입력: ').split()
+x, y = 1, 1
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+move_types = ['R', 'L', 'U', 'D']
+
+for plan in plans:
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+    	continue
+        
+    x,y = nx, ny
+
+print('모험가 최종 좌표 : ', x, y)
